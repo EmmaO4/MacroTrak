44312462dece7feb33d02429382fe02929dfb4f4
Thom Null <raulesquerra18@gmail.com>
2026-02-27 14:23:48 -0800

    init commit

---
+++ food.py
@@ -0,0 +1,19 @@
+class Food:
+    def __init__(self, calories, fat, sat_fat, poly_fat, mono_fat, carbs, fiber, insol_fiber, sol_fiber, sugar, protein):
+        self.calories = calories
+        self.fat = fat
+        self.sat_fat = sat_fat
+        self.poly_fat = poly_fat
+        self.mono_fat = mono_fat
+        self.carbs = carbs
+        self.fiber = fiber
+        self.insol_fiber = insol_fiber
+        self.sol_fiber = sol_fiber
+        self.sugar = sugar
+        self.protein = protein
+
+    def add_food(self):
+        pass
+
+    def del_food(self):
+        pass
